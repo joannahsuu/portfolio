31eb902dbd7cf4d30dc5de49e4c0821efdbbe33e
許靖玟 <Joanna.Hus@want-want.com>
2019-05-22 11:25:28 +0800

    打包測試

---
+++ dist/precache-manifest.a04832b0420082998df5432dbb747e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "14dde2b2d50728fa7600",
+    "url": "js/chunk-vendors.f3e6a31b.js"
+  },
+  {
+    "revision": "48b134e328cbc356b2e5",
+    "url": "js/app.c000d51f.js"
+  },
+  {
+    "revision": "13ac515298e54042c806ba6ac98a61d0",
+    "url": "index.html"
+  },
+  {
+    "revision": "e8943f71af3fc1a0572116c9dc2e00ee",
+    "url": "img/profile_meow.e8943f71.svg"
+  },
+  {
+    "revision": "b3576e85aaff31b09b845e85300c5ae8",
+    "url": "img/bg_main.b3576e85.jpeg"
+  },
+  {
+    "revision": "48b134e328cbc356b2e5",
+    "url": "css/app.6ab50eea.css"
+  }
+];
